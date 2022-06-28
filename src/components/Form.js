4fe9da62c992516ae9b128c0ccea3756e811cbba
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-28 18:29:22 +0100

    Created the Form Component

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+function Form() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Dictionary Project</p>
+        </header>
+      </div>
+    );
+}
+
+export default Form;
