f7c71332ffee5dcafa5af33690a67bfc46389d91
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-30 09:14:33 +0100

    Modieified the App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Form from './components/Form';
+import Navbar from './components/Navbar';
+import Details from './components/Details';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Form />
+      <Details />
+      <Footer />
     </div>
   );
 }
